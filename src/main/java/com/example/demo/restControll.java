0a5a7c9f43ec726490a857a4ea8122fd124f4a28
tcbd17094 <tcbd17094@BVB1399.InternetScourt.go.kr>
2017-12-18 17:25:47 +0900

    서버 설정

---
+++ src/main/java/com/example/demo/restControll.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restControll {
+
+	@Value("${greeting}")
+	private String greeting;
+	
+	@RequestMapping(value="/come", method=RequestMethod.GET)
+	public String com(){
+		return greeting.replace("\"", "");
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public String page(){
+		return "<html><body><button id='dd' value='dfdf'></button></body></html>";
+	}
+	
+}
